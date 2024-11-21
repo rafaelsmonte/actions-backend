b4635f8859e7d8ce0fa4882af1a4f6cb0594d746
Rafael Santos Monte <rsmonte@hotmail.com>
2024-11-20 22:09:17 -0300

    test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["json-summary", "text", "lcov"],
   testEnvironment: "node",
   transform: {
     "^.+.tsx?$": ["ts-jest",{}],
